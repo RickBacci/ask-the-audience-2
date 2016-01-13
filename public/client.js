2388dff6a11b0d8c9a9a947064e9fdbdcf538b86
Rick Bacci <rickbacci@gmail.com>
2016-01-13 11:23:26 -0700

    initial commit

---
+++ public/client.js
@@ -0,0 +1,9 @@
+var socket = io();
+
+var connectionCount = document.getElementById('connection-count');
+
+socket.on('usersConnected', function(count) {
+  connectionCount.innerText = 'Connected Users: ' + count;
+});
+
+
